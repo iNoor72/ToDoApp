c2487939dc3985be00060e0185766eef09bde2d2
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-25 23:37:07 +0200

    Now the app works very fine with the categories. Next step: The items. 💪

--- MyTodoApp/MainView.swift
+++ MyTodoApp/MainView.swift
@@ -8,50 +8,84 @@
 import UIKit
 import CoreData
 
-class MainView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainView: UIViewController {
     
 
-    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     let context = (UIApplication.shared.delegate as! AppDelegate).CoreDataDatabase.viewContext
-    var items = [Item]()
-    var categories = Categories()
+    
+    var categories = [Categories]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getCategories()
         tableView.delegate = self
         tableView.dataSource = self
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.backButtonTitle = "Categories"
+        categoryTextField.resignFirstResponder()
     }
     
     @IBAction func addButtonPressed(_ sender: UIButton) {
-        if let newItem = itemTextField.text {
-            items.append(Item(context: context))
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-            
-            //Add to Core Data
+        if let categoryName = categoryTextField.text {
+            let newCategory = Categories(context: context)
+            newCategory.name = categoryName
+            categories.append(newCategory)
+            savingContext()
+            getCategories()
         }
     }
     
-   
+    
+    //MARK:- Core Data Functions
+    
+    func getCategories() {
+        do {
+            categories = try context.fetch(Categories.fetchRequest())
+        }catch {
+            print("There was an error fetching request of Categories class. \(error.localizedDescription)")
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func deleteCategory(named category: Categories){
+        context.delete(category)
+        savingContext()
+        getCategories()
+    }
+    
+    func savingContext() {
+        do {
+            try context.save()
+        }catch {
+            print("There was a problem while saving new context. \(error.localizedDescription)")
+        }
+    }
+    
+}
+
+    //MARK:- TableView Delegate & DataSource Methods
+
+extension MainView : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-//        return 5
+        return categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "\(items[indexPath.row].name)"
+        cell.textLabel?.text = categories[indexPath.row].name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let destination = storyboard?.instantiateViewController(identifier: "") as! ItemViewController
-        //destination.title = categories.items[indexPath.row].name
+        let destination = storyboard?.instantiateViewController(identifier: "ItemViewController") as! ItemViewController
+        let currentCategory = categories[indexPath.row]
+        destination.category = currentCategory
+        navigationController?.pushViewController(destination, animated: true)
     }
-
-    
 }
 
--- MyTodoApp/View/ToDoItemCell.swift
+++ MyTodoApp/View/ToDoItemCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ToDoItemCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
